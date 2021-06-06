348bf2b496792960d8a6ae74aa51da97d483bfa1
Andy <andy27479@gmail.com>
2021-06-06 14:58:56 +0800

    initial commit

---
+++ js_task/js/video.js
@@ -0,0 +1,11 @@
+var video;
+
+window.addEventListener("load", function() {
+	console.log("Good job opening the window")
+
+});
+
+// document.querySelector("#play").addEventListener("click", function() {
+// 	console.log("Play Video");
+// });
+
