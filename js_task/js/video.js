637a5118ccbc28c5b32c7b8be686471ee1dc0921
Andy <andy27479@gmail.com>
2021-06-10 02:09:25 +0800

    finished all eventListeners v1

--- js_task/js/video.js
+++ js_task/js/video.js
@@ -1,11 +1,56 @@
-var video;
+const video = document.querySelector('video');
+const play = document.querySelector('#play');
+const pause = document.querySelector('#pause');
+const slower = document.querySelector('#slower');
+const faster = document.querySelector('#faster');
+const skip = document.querySelector('#skip');
+const mute = document.querySelector('#mute');
+const volume = document.querySelector('#volume');
+const slider = document.querySelector('#slider');
+const vintage = document.querySelector('#vintage');
+const orig = document.querySelector('#orig');
 
-window.addEventListener("load", function() {
-	console.log("Good job opening the window")
+function playVideo() {
+	video.play();
+	volume.textContent = slider.value;
+}
 
-});
+function slowerVideo() {
+	video.playbackRate = video.playbackRate * 0.95;
+	console.log(video.playbackRate);
+}
 
-// document.querySelector("#play").addEventListener("click", function() {
-// 	console.log("Play Video");
-// });
+function fasterVideo() {
+	video.playbackRate = video.playbackRate * 1.05;
+	console.log(video.playbackRate);
+}
 
+function skipVideo() {
+	const duration = video.duration;
+	if (video.currentTime + 15 > duration) {
+		video.currentTime = 0;
+	} else {
+		video.currentTime += 15;
+	}
+	console.log(video.currentTime);
+}
+
+function toggleMute() {
+	video.muted = video.muted ? false : true;
+	mute.textContent = video.muted ? 'Unmute' : "Mute";
+}
+
+function handleVolume() {
+	video.volume = video.volume * slider.value * 0.01;
+	volume.textContent = slider.value;
+}
+
+play.addEventListener("click", playVideo);
+pause.addEventListener("click", () => video.pause());
+slower.addEventListener("click", slowerVideo);
+faster.addEventListener("click", fasterVideo);
+skip.addEventListener("click", skipVideo);
+mute.addEventListener("click", toggleMute);
+slider.addEventListener("mousemove", handleVolume);
+vintage.addEventListener("click", () => video.classList.add("oldSchool"));
+orig.addEventListener("click", () => video.classList.remove("oldSchool"));
